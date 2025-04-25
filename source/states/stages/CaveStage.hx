524ad9f08e8f53768208c938e264932039490de2
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-25 06:45:07 -0400

    Update CaveStage.hx

--- source/states/stages/CaveStage.hx
+++ source/states/stages/CaveStage.hx
@@ -8,7 +8,7 @@ import shaggymodchart.ModScript;
 
 class CaveStage
 
-function create() {
+function create():Void {
     var bg = sprite_load("cavebg/bg");
     offset_setby_fraction(bg, 0.5, 0.5);
     var super_bg = sprite_load("cavebg/super_bg");
